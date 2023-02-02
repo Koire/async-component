70812296d4120bd98f6326e712106e205e8649bf
Koire <pikewb@gmail.com>
2023-02-02 10:13:55 +0900

    updated with an example

---
+++ example/src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import {AsyncComponentCreator} from "../../src/index"
+
+function App() {
+  const [result, setResult] = useState("")
+  const { AsyncComponent, asyncGetValue, cancel, confirm } = AsyncComponentCreator()
+  const cancelFn = () => cancel(setResult("user cancelled"))
+  const confirmFn = () => confirm(setResult("user confirmed"))
+  return (
+    <div className="App">
+      <h1>Async Component </h1>
+      <p>{result}</p>
+        <AsyncComponent>
+          <div className="modal">
+            <div>  Do you accept the terms and conditions</div>
+            <div>
+              <button onClick={cancelFn}>Cancel</button>
+              <button onClick={confirmFn}>Confirm</button>
+            </div>
+          </div>
+        </AsyncComponent>
+      <button onClick={asyncGetValue}>Get Value</button>
+    </div>
+  )
+}
+
+export default App
